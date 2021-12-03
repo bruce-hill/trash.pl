8681be9e8b6dbb1df07196021bc98b7aa2e259a4
Bruce Hill <bruce@bruce-hill.com>
2021-12-02 18:48:23 -0800

    Improvements

--- bin.pl
+++ bin.pl
@@ -1,15 +1,16 @@
-#!/usr/bin/perl -l
+#!/usr/bin/perl
 #
 # Command line interface for Freedesktop.org-compliant trash
 #
+use feature say;
 use URI::Escape;
 use Getopt::Long;
 use Time::Ago;
-use Time::Piece;
 use IPC::Open3;
-use File::Copy;
+use Time::Piece;
 use File::Temp qw/ tempfile /;
 use Cwd qw(abs_path);
+use POSIX qw(strftime);
 
 Getopt::Long::Configure("bundling");
 
@@ -18,6 +19,8 @@ my $help = "";
 my $untrash = "";
 my $list = "";
 my $empty = "";
+my $interactive = "";
+my $force = "";
 
 GetOptions(
     "verbose|v" => \$verbose,
@@ -25,47 +28,65 @@ GetOptions(
     "untrash|u" => \$untrash,
     "list|l" => \$list,
     "empty|e" => \$empty,
+    "interactive|i" => \$interactive,
+    "force|f" => \$force,
 );
 
+sub confirm {
+    print "@_[0] [y/N] ";
+    chomp(my $reply = <STDIN>);
+    if ($reply ne "y") {
+        exit 1;
+    }
+}
+
 if ($help) {
-    print "Help";
+    say "bin.pl - Command-line trash";
     exit 0;
 }
 
 if ($empty) {
-    print "Emptying...";
+    confirm "Empty the trash?" if $interactive;
+    say "Emptying..." if $verbose;
     for (glob "~/.Trash/files/*") {
-        print if $verbose;
+        say if $verbose;
         unlink;
     }
     for (glob "~/.Trash/info/*") {
         unlink;
     }
+    say "Trash emptied!";
 } elsif ($untrash) {
     @files=();
     my $i = 0;
     for (glob "~/.Trash/info/*") {
         open(my $info, "<", $_);
-        @lines = <$info>;
+        my @lines = <$info>;
         (my $path) = ($lines[1] =~ /^Path=(.*)/);
         $path = uri_unescape($path);
         (my $date) = ($lines[2] =~ /^DeletionDate=(.*)/);
-        $date = Time::Piece->strptime($date, "%FT%H:%M:%S");
+        $date = localtime->strptime($date, "%FT%H:%M:%S");
         # (my $deleted) = ($_ =~ s@/info/(.*)\.trashinfo$@/files/$1@);
         (my $deleted) = /([^\/]+)\.trashinfo$/;
         push(@files, {i => $i++, trashinfo => $_, path => $path, date => $date, deleted => $deleted})
+    }
+
+    if (!@files) {
+        say "No files currently in the trash.";
+        exit 1;
     }
 
     my $pid = open3(my $fzf_in, my $fzf_out, ">&STDERR",
         "fzf", "-d", '\\t', "--nth=2..", "--with-nth=2..", "-m", "-1", "-0", "-q", "@ARGV");
     for (sort {$b->{date} <=> $a->{date}} @files) {
         $ago = Time::Ago->in_words($_->{date});
-        print $fzf_in "$_->{i}\t$ago ago\t$_->{path}";
+        say $fzf_in "$_->{i}\t$ago ago\t$_->{path}";
     }
     close($fzf_in);
 
     while ((my $i) = <$fzf_out> =~ /^(\d+)/) {
-        print "Restoring: $ENV{HOME}/.Trash/files/$files[$i]->{deleted} -> $files[$i]->{path}" if $verbose;
+        confirm "Restore $files[$i]->{path}?" if $interactive;
+        say "Restoring: $ENV{HOME}/.Trash/files/$files[$i]->{deleted} -> $files[$i]->{path}" if $verbose;
         rename "$ENV{HOME}/.Trash/files/$files[$i]->{deleted}", $files[$i]->{path};
         unlink $files[$i]->{trashinfo};
     }
@@ -77,29 +98,37 @@ if ($empty) {
 } elsif ($list) {
     for (glob "~/.Trash/info/*") {
         open(my $info, "<", $_);
-        @lines = <$info>;
+        my @lines = <$info>;
         (my $path) = ($lines[1] =~ /^Path=(.*)/);
         $path = uri_unescape($path);
         (my $date) = ($lines[2] =~ /^DeletionDate=(.*)/);
-        $date = Time::Piece->strptime($date, "%FT%H:%M:%S");
-        $ago = Time::Ago->in_words($date);
-        print "$date\t$path";
+        $date = localtime->strptime($date, "%FT%H:%M:%S");
+        my $ago = Time::Ago->in_words($date);
+        say "$ago ago\t$path";
     }
 } else {
-    print "Deleting..." if $verbose;
+    say "Trashing..." if $verbose;
     for (@ARGV) {
         if (! -e $_) {
-            print "File does not exist: $_";
+            say "File does not exist: $_";
             exit 1;
         }
+        if ($interactive) {
+            confirm "Send to trash: $_?" if $interactive;
+            # print "Delete $_? [y/N] ";
+            chomp(my $reply = <STDIN>);
+            if ($reply ne "y") {
+                exit 1;
+            }
+        }
         my ($f, $filename) = tempfile("$ENV{HOME}/.Trash/info/$_-XXXXXX", SUFFIX => ".trashinfo");
-        print $f "[Trash Info]";
+        say $f "[Trash Info]";
         my $path = abs_path($_);
-        print $path if $verbose;
+        say $path if $verbose;
         $path =~ s;([^/]+);uri_escape($1);eg;
-        print $f "Path=$path";
-        my $date = localtime->strftime("%FT%H:%M:%S");
-        print $f "DeletionDate=$date";
+        say $f "Path=$path";
+        my $date = strftime("%FT%H:%M:%S", localtime);
+        say $f "DeletionDate=$date";
         close($f);
         my $dest = $filename =~ s;/info/([^/]*)\.trashinfo$;/files/$1;r;
         rename $_, $dest;
